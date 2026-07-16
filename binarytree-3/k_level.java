d15339a98665f9527d773777d212b09204847ef6
Yashiinde <yashrajshinde056@gmail.com>
2026-07-16 22:15:07 +0530

    new approch for lowest common anccestor

--- binarytree-3/k_level.java
+++ binarytree-3/k_level.java
@@ -53,6 +53,20 @@ public class k_level {
         Node lca =path1.get(i-1);
         return lca;
     }
+    public static Node lca2(Node root, int n1, int n2){
+        if(root==null || root.data==n1 || root.data==n2){
+            return root;
+        }
+        Node leftlca=lca2(root.left, n1, n2);
+        Node rightlca=lca2(root.right, n1, n2);
+        if(rightlca==null){
+            return leftlca;
+       }
+       if(leftlca==null){
+        return rightlca;
+       }
+       return root;
+    }
     public static void main(String[] args) {
        Node root= new Node(1);
        root.left=new Node(2);
@@ -61,6 +75,6 @@ public class k_level {
        root.left.right= new Node(5);
        root.right.left= new Node(6);
        root.right.right= new Node(7);
-       System.out.println(lca(root, 4, 3).data);
+       System.out.println(lca2(root, 4, 5).data);
     }
 }
