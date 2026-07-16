7f712898aaf89ff2f42f3de0b42e08985e3f2261
Yashiinde <yashrajshinde056@gmail.com>
2026-07-16 21:32:37 +0530

    new tree problems added

---
+++ binarytree-3/k_level.java
@@ -0,0 +1,66 @@
+import java.util.ArrayList;
+
+public class k_level {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        public Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+
+        }
+    }
+    public static void klevel(Node root,int level,int k){
+        if(root==null){
+            return;
+        }
+        if(level==k){
+            System.out.print(root.data+" ");
+            return;
+        }
+        klevel(root.left, level+1, k);
+        klevel(root.right, level+1, k);
+    }
+    public static boolean getpath(Node root, int n, ArrayList<Node> path){
+        if(root==null){
+            return false;
+        }
+        path.add(root);
+        if(root.data==n){
+            return true;
+        }
+        boolean foundleft=getpath(root.left, n, path);
+        boolean foundright=getpath(root.right, n, path);
+        if(foundleft || foundright){
+            return true;
+        }
+        path.remove(path.size()-1);
+        return false;
+    }
+    public static Node lca(Node root, int n1, int n2){
+        ArrayList<Node> path1= new ArrayList<>();
+        ArrayList<Node> path2=new ArrayList<>();
+        getpath(root, n1, path1);
+        getpath(root, n2, path2);
+        int i=0;
+        for(;i<path1.size() && i<path2.size();i++){
+            if(path1.get(i)!=path2.get(i)){
+                break;
+            }
+        }
+        Node lca =path1.get(i-1);
+        return lca;
+    }
+    public static void main(String[] args) {
+       Node root= new Node(1);
+       root.left=new Node(2);
+       root.right= new Node(3);
+       root.left.left= new Node(4);
+       root.left.right= new Node(5);
+       root.right.left= new Node(6);
+       root.right.right= new Node(7);
+       System.out.println(lca(root, 4, 3).data);
+    }
+}
